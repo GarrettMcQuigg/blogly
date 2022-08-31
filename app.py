f63f0b7b230fa402b8c84c64ef05fce6ee1c71c9
Garrett McQuigg <garrettmcquigg@gmail.com>
2022-08-31 14:31:59 -0500

    2

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 """Blogly application."""
 from crypt import methods
-from flask import Flask, render_template, redirect, session, request
-from models import db, connect_db, User
+from flask import Flask, render_template, redirect, session, request, flash
+from models import db, connect_db, User, Post
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
@@ -62,3 +62,64 @@ def users_update(user_id):
     db.session.commit()
 
     return redirect("/users")
+
+@app.route('/<int:user_id>/posts/new')
+def post_form(user_id):
+
+    user = User.query.get_or_404(user_id)
+    return render_template('posts/new.html', user=user)
+
+
+@app.route('/<int:user_id>/posts/new', methods=["POST"])
+def new_posts(user_id):
+
+    user = User.query.get_or_404(user_id)
+    new_post = Post(title=request.form['title'],
+                    content=request.form['content'],
+                    user=user)
+
+    db.session.add(new_post)
+    db.session.commit()
+    flash(f"Post '{new_post.title}' added.")
+
+    return redirect(f"/{user_id}")
+
+
+@app.route('/posts/<int:post_id>')
+def posts_show(post_id):
+
+    post = Post.query.get_or_404(post_id)
+    return render_template('posts/show.html', post=post)
+
+
+@app.route('/posts/<int:post_id>/edit')
+def posts_edit(post_id):
+
+    post = Post.query.get_or_404(post_id)
+    return render_template('posts/edit.html', post=post)
+
+
+@app.route('/posts/<int:post_id>/edit', methods=["POST"])
+def posts_update(post_id):
+    
+    post = Post.query.get_or_404(post_id)
+    post.title = request.form['title']
+    post.content = request.form['content']
+
+    db.session.add(post)
+    db.session.commit()
+    flash(f"Post '{post.title}' edited.")
+
+    return redirect(f"/users/{post.user_id}")
+
+
+@app.route('/posts/<int:post_id>/delete', methods=["POST"])
+def posts_destroy(post_id):
+    
+    post = Post.query.get_or_404(post_id)
+
+    db.session.delete(post)
+    db.session.commit()
+    flash(f"Post '{post.title} deleted.")
+
+    return redirect(f"/users/{post.user_id}")
--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 """Models for Blogly."""
+import datetime
 from email.policy import default
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy, func
 
 db = SQLAlchemy()
 
@@ -32,3 +33,25 @@ class User(db.Model):
 def connect_db(app):
     db.app = app
     db.init_app(app)
+
+
+class Post(db.Model):
+    """User Posts"""
+
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+
+    title = db.Column(db.Text(100),
+                      nullable=False)
+
+    content = db.Column(db.String,
+                        nullable=False)
+
+    created_at = db.Column(db.DateTime,
+                           nullable=False,
+                           default=datetime.datetime.now)
+
+    user_code = db.Column(db.Text, db.ForeignKey('users.id'))
